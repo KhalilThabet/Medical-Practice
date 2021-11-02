c037a2e63e117000312d24c1f5d6b86cb4d3a64b
Khalil Thabet <khalilthabet@outlook.com>
2021-11-02 22:40:20 +0100

    Corrections On Input

--- MedicalPractice.py
+++ MedicalPractice.py
@@ -452,16 +452,16 @@ def M_P(): #fonction du prgramme principale
  import os
  import time
  M_P_L()#appelle a la fonction Look du programme
- dict={'1':Creation_fichier_patient,
-      '2':ajouter_Patient,
-      '3':Supprimer_patient,
-      '4':Ajouter_RDV,
-      '5':Annuler_RDV,
-     '6':Modify_RDV,
-       '7':Ordonnance,
-       '8':Historique_patient,
-       '9':Consulation_Mois,
-       '10':Consultation_An}#dictionnaire contenant les fonctions du programme exigees
+ dict={'1':"Creation_fichier_patient",
+      '2':"ajouter_Patient",
+      '3':"Supprimer_patient",
+      '4':"Ajouter_RDV",
+      '5':"Annuler_RDV",
+     '6':"Modify_RDV",
+       '7':"Ordonnance",
+       '8':"Historique_patient",
+       '9':"Consulation_Mois",
+       '10':"Consultation_An"}#dictionnaire contenant les fonctions du programme exigees
  print("")
  c=input()
  dict[c]()
