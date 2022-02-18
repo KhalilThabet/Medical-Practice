698521efddef2677b775a1ce940f73f03e7ddd99
kahlilthabet <khalilthabet@outlook.com>
2022-02-19 00:48:18 +0100

    Closing the test Work

--- Src/M_P.py
+++ Src/M_P.py
@@ -1,14 +1,14 @@
 import sys
-
+import os
+os.environ["SDL_VIDEODRIVER"]="x11"
 sys.path.insert(1, "../")
 from Src.M_P_L import M_P_L
 from Handlers.Ajouter_Patient import ajouter_Patient
 from Handlers.Ajouter_RDV import Ajouter_RDV
 from Handlers.Annuler_RDV import Annuler_RDV
-
-# from Handlers.Consultation_An import Consultation_An
-# from Handlers.Consultation_Mois import Consulation_Mois
-# from Handlers.Historique_Patient import Historique_patient
+from Handlers.Consultation_An import Consultation_An
+from Handlers.Consultation_Mois import Consulation_Mois
+from Handlers.Historique_Patient import Historique_patient
 from Handlers.Ordonnanceur import Ordonnance
 from Handlers.Supprimer_Patient import Supprimer_patient
 from Handlers.Modifier_RDV import Modifier_RDV
@@ -26,9 +26,9 @@ def M_P():  # fonction du prgramme principale
         "5": Annuler_RDV,
         "6": Modifier_RDV,
         "7": Ordonnance,
-        "8": "Historique_patient",
-        "9": "Consulation_Mois",
-        "10": "Consultation_An",
+        "8": Historique_patient,
+        "9": Consulation_Mois,
+        "10": Consultation_An,
     }  # dictionnaire contenant les fonctions du programme exigees
     print("")
     c = input()
