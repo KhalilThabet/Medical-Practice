e95d554828fff80abdd42d724fd722eb015cf5d3
kahlilthabet <khalilthabet@outlook.com>
2022-02-18 23:45:14 +0100

    Adding getch module from another Repository

---
+++ getch/getch.py
@@ -0,0 +1,14 @@
+def getch():
+        """
+        Gets a single character from STDIO.
+        """
+        import sys
+        import tty
+        import termios
+        fd = sys.stdin.fileno()
+        old = termios.tcgetattr(fd)
+        try:
+            tty.setraw(fd)
+            return sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old)
